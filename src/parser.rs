3a8ab21c6dca4a7f9d40c574ba516beb9bafe8ac
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2021-12-18 05:47:08 +0900

    better parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,19 @@
-use nom::{branch::*, character::complete::*, combinator::*, Err, error::Error, sequence::*, IResult, bytes::complete::tag};
+use nom::{
+  branch::*, bytes::complete::*, character::complete::*, combinator::*, error::Error, sequence::*,
+  Err, IResult,
+};
 
 use super::logic::*;
 
 pub type ParseLogicError = Err<Error<String>>;
 
 /*
-  <expr>    := <term> [ [ ws ] '\to' ws <term>]
-  <term>    := <factor> [ [ ws ] '\land'|'\lor' ws <factor> ]
-  <factor>  := [ '\lnot' ws ] ( <base> | <paren> )
-  <paren>   := '(' [ ws ] <expr> [ ws ] ')'
+  <to>      := ( <and> | <or> ) [ ws0 '\to ' ws0 <to> ]
+  <and>     := <not> [ ws0 '\land ' ws0 <and> ]
+  <or>      := <not> [ ws0 '\lor ' ws0 <or> ]
+  <not>     := [ '\lnot ' ws0 ] ( <base> | <cont> | <paren> | <not> )
+  <paren>   := '(' ws0 <to> ws0 ')'
+  <cont>    := '\perp '
   <base>    := A-Z
 */
 
@@ -16,74 +21,74 @@ fn base(s: &str) -> IResult<&str, Logic> {
   map(one_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), |c| Logic::Base(c))(s)
 }
 
+fn cont(s: &str) -> IResult<&str, Logic> {
+  value(Logic::Cont, alt((tag("\\perp "), tag("cont"), tag("⊥"))))(s)
+}
+
 fn paren(s: &str) -> IResult<&str, Logic> {
   delimited(
     char('('),
-    delimited(multispace0, expr, multispace0),
+    delimited(multispace0, parse, multispace0),
     char(')'),
   )(s)
 }
 
-fn factor(s: &str) -> IResult<&str, Logic> {
-  map(
-    tuple((
-      opt(tuple((
-        alt((tag("\\lnot"), tag("not"), tag("¬"))),
-        multispace1,
-      ))),
-      alt((base, paren, factor)),
-    )),
-    |(opt, e)| match opt {
-      Some(_) => Logic::Not(Box::new(e)),
-      None => e,
-    },
-  )(s)
-}
-
 fn term(s: &str) -> IResult<&str, Logic> {
-  map(
-    tuple((
-      factor,
-      opt(tuple((
-        multispace0,
-        alt((
-          map(tag("\\land"), |_| true),
-          map(tag("and"), |_| true),
-          map(tag("∧"), |_| true),
-          map(tag("\\lor"), |_| false),
-          map(tag("or"), |_| false),
-          map(tag("∨"), |_| false),
-        )),
-        multispace1,
-        factor,
-      ))),
-    )),
-    |(f0, opt)| match opt {
-      Some((_, t, _, f1)) => match t {
-        true => Logic::And(Box::new(f0), Box::new(f1)),
-        false => Logic::Or(Box::new(f0), Box::new(f1)),
-      },
-      None => f0,
-    },
-  )(s)
+  alt((base, cont, paren, not))(s)
 }
 
-pub fn expr(s: &str) -> IResult<&str, Logic> {
+fn not(s: &str) -> IResult<&str, Logic> {
   map(
     tuple((
-      opt(tuple((
-        term,
-        multispace0,
-        alt((tag("\\to"), tag("\\to"), tag("→"))),
-        multispace1,
-      ))),
+      alt((tag("\\lnot "), tag("not"), tag("¬"))),
+      multispace0,
       term,
     )),
-    |(opt, e)| match opt {
-      Some(t) => Logic::To(Box::new(t.0), Box::new(e)),
-      None => e,
-    },
+    |t| Logic::Not(Box::new(t.2)),
   )(s)
+}
+
+fn and(s: &str) -> IResult<&str, Logic> {
+  map(
+    tuple((
+      term,
+      multispace0,
+      alt((tag("\\land "), tag("and"), tag("∧"))),
+      multispace0,
+      alt((and, term)),
+    )),
+    |t| Logic::And(Box::new(t.0), Box::new(t.4)),
+  )(s)
+}
+
+fn or(s: &str) -> IResult<&str, Logic> {
+  map(
+    tuple((
+      term,
+      multispace0,
+      alt((tag("\\lor "), tag("or"), tag("∨"))),
+      multispace0,
+      alt((or, term)),
+    )),
+    |t| Logic::Or(Box::new(t.0), Box::new(t.4)),
+  )(s)
+}
+
+fn to(s: &str) -> IResult<&str, Logic> {
+  map(
+    tuple((
+      alt((and, or, term)),
+      multispace0,
+      alt((tag("\\to "), tag("to"), tag("→"))),
+      multispace0,
+      parse,
+    )),
+    |t| Logic::To(Box::new(t.0), Box::new(t.4)),
+  )(s)
+}
+
+pub fn parse(s: &str) -> IResult<&str, Logic> {
+  alt((to, and, or, term))(s)
 }
 
 #[cfg(test)]
@@ -102,29 +107,38 @@ mod test {
   }
 
   #[test]
-  fn test_factor() {
-    assert_eq!(factor("A").unwrap(), ("", Base('A')));
-    assert_eq!(factor("\\lnot A").unwrap(), ("", Not(Box::new(Base('A')))));
+  fn test_not() {
+    assert_eq!(not("\\lnot A").unwrap(), ("", Not(Box::new(Base('A')))));
   }
 
   #[test]
-  fn test_term() {
-    assert_eq!(term("\\lnot A").unwrap(), ("", Not(Box::new(Base('A')))));
+  fn test_and() {
     assert_eq!(
-      term("A \\land B").unwrap(),
+      and("A \\land B").unwrap(),
       ("", And(Box::new(Base('A')), Box::new(Base('B'))))
     );
+  }
+
+  #[test]
+  fn test_or() {
     assert_eq!(
-      term("A \\lor B").unwrap(),
+      or("A \\lor B").unwrap(),
       ("", Or(Box::new(Base('A')), Box::new(Base('B'))))
     );
   }
 
   #[test]
-  fn test_expr() {
-    assert_eq!(expr("\\lnot A").unwrap(), ("", Not(Box::new(Base('A')))));
+  fn test_to() {
     assert_eq!(
-      expr("(A \\lor B \\to C) \\to ((A \\to C) \\land (B \\to C))").unwrap(),
+      to("A \\to B").unwrap(),
+      ("", To(Box::new(Base('A')), Box::new(Base('B'))))
+    );
+  }
+
+  #[test]
+  fn test_parse() {
+    assert_eq!(
+      parse("(A \\lor B \\to C) \\to ((A \\to C) \\land (B \\to C))").unwrap(),
       (
         "",
         To(
@@ -138,6 +152,16 @@ mod test {
           ))
         )
       )
-    )
+    );
+    assert_eq!(
+      parse("A to not not A").unwrap(),
+      (
+        "",
+        To(
+          Box::new(Base('A')),
+          Box::new(Not(Box::new(Not(Box::new(Base('A'))))))
+        )
+      )
+    );
   }
 }
